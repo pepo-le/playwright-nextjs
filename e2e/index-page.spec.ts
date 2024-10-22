60ac2761278ff42ba2b6475c265114224650307a
pepo-le <hello@pepo-le.com>
2024-10-22 20:43:13 +0900

    Initial commit

---
+++ e2e/index-page.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('Shall render hello world', async ({ page }) => {
+  await page.goto('/')
+  await expect(page).toHaveTitle('e2e test')
+  await expect(page.getByRole('heading')).toHaveText('hello world')
+})
