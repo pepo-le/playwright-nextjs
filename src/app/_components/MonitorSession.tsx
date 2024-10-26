b0f702db91d3452e59d8b446a4c01b84db775a28
pepo-le <hello@pepo-le.com>
2024-10-26 19:04:00 +0900

    ナビを追加

---
+++ src/app/_components/MonitorSession.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+
+export default function MonitorSession() {
+  const router = useRouter()
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    if (!session) {
+      router.refresh()
+    }
+  }, [session])
+
+  return null
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Provider from "./provider";
+import NavBar from "./_components/NavBar";
+import MonitorSession from "./_components/MonitorSession";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          <NavBar />
+          <MonitorSession />
+          {children}
+        </Provider>
       </body>
     </html>
   );
